3d124abb5d37f1efb1cdf13c657c89942474ea08
Ayush <ayushjoshi1079@gmail.com>
2026-09-02 22:49:34 +0530

    in memory cache implemented

---
+++ source_code/InMemoryCache.py
@@ -0,0 +1,31 @@
+from collections import OrderedDict
+import threading
+
+class InMemoryCache:
+    def __init__(self , capacity):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+        self.lock = threading.Lock()
+        
+    def get(self , key):
+        with self.lock:
+            if key not in self.cache:
+                print(f'Key {key} Not found')
+                return None
+            self.cache.move_to_end(key)
+            print(f'Get : {key} -> {self.cache[key]}')
+            return self.cache[key]
+        
+    def put(self , key , value):
+        with self.lock:
+            if key in self.cache:
+                self.cache.move_to_end(key)
+            self.cache[key] = value
+            if len(self.cache) > self.capacity:
+                removed = self.cache.popitem(last = False)
+                print(f'Evicted (LRU) : {removed}')
+            print(f'PUT : {key} -> {value}')
+            
+    def display(self):
+        with self.lock:
+            print(f'Current cache status -> {dict(self.cache)}')
